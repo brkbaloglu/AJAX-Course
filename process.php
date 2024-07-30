fb77cf9bd9261f40ddc2adffead300cada83150e
Mehmet Burak Baloğlu <burakbaloglu148@gmail.com>
2024-07-30 18:27:38 +0300

    first commit

---
+++ process.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Processing...";
+
+
+if (isset($_GET["name"])) {
+    echo "GET: Your name is ".$_GET["name"];
+}
+
+if (isset($_POST["name"])) {
+    echo "POST: Your name is ".$_POST["name"];
+}
+
+
+
+?>
